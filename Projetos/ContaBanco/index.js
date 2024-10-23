0ecab7776746080f579e608f4997e53cd42f10d5
Renato Matsuo <renatomatsuo@icloud.com>
2024-10-22 21:33:12 -0300

    Atualização de alguns arquivos

--- Projetos/ContaBanco/index.js
+++ Projetos/ContaBanco/index.js
@@ -5,33 +5,40 @@ function Cliente(name, age, user, password, saldo = 0) {
     password = password,
     this.saldo = saldo,
 
-    this.depositar = function(valorDepositado, saldo) {
+    this.depositar = function(usuario, senha, valorDepositado, saldo) {
+        /*CONTINUAR DAQUI!!*/ const usuario = prompt("Insira seu usuário:")
+        let senha = prompt("Insira sua senha:")
         if (valorDepositado > 0 && valorDepositado != NaN) {
-            saldo = this.saldo += valorDepositado
-            console.log(`Depósito no valor de R$${valorDepositado},00 realizado com sucesso!`)
-            console.log(`Seu novo saldo é de R$${this.saldo},00.`)
-        } else {
-            (valorDepositado <= 0)
+                if (usuario == this.user && senha == this.password) { 
+                saldo = this.saldo += valorDepositado
+                console.log(`Depósito no valor de R$${valorDepositado},00 realizado com sucesso!`)
+                console.log(`Seu novo saldo é de R$${this.saldo},00.`)
+            } else {
+                (valorDepositado <= 0)
                 console.log('Valor de depósito inválido.')
-        }
-    
-    this.mostrarSaldo = (saldo) => console.log(`Seu saldo é de R$${this.saldo},00.`)
-    
+            }
+        }    
+    }
+
+    this.mostrarSaldo = saldo => console.log(`Seu saldo é de R$${this.saldo},00.`)
+
     this.sacar = function (valorASacar, saldo) {
         if (valorASacar <= this.saldo && valorASacar > 0 && valorASacar != NaN) {
-            let saldo = this.saldo -= valorASacar 
+            saldo = this.saldo -= valorASacar 
             console.log('Saque foi realizado com sucesso!')
             console.log(`Seu saldo atual é de R$${this.saldo},00.`)
         } else {
             console.log('Saldo insulficiente ou valor inválido.')    
         }
-        }
     }
 }
 
+
 let Cliente1 = new Cliente(nome = 'Renato Matsuo', age = 32, user = 'renatoomatsuo', password = '1234')
 
+Cliente1.mostrarSaldo()
 Cliente1.depositar(500)
+Cliente1.mostrarSaldo()
 Cliente1.depositar('afa')
 Cliente1.depositar('a')
 Cliente1.depositar(0)
